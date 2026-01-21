5a1009abf8874a6bf19597f19bb524ed86fe97c6
george <rqzrqh@163.com>
2026-01-21 14:11:15 +0800

    optimize task manager

--- fetch/task_manager.go
+++ fetch/task_manager.go
@@ -1,35 +1,36 @@
 package fetch
 
 import (
-	"container/list"
 	"fmt"
 	"os"
 
+	"github.com/elliotchance/orderedmap/v3"
 	"golang.org/x/xerrors"
 )
 
 type TaskManager struct {
-	maxTaskCount int
-	taskId       int
-	pendingFetch *list.List // soft in height
-	inFetch      map[uint64]struct{}
-	inStage      map[uint64]struct{}
+	maxUnorganizedBlockCount int
+	taskId                   int
+	pendingFetch             *orderedmap.OrderedMap[uint64, struct{}]
+	inFetch                  map[uint64]struct{}
+	inStage                  map[uint64]struct{}
 }
 
-func NewTaskManager(maxTaskCount int) *TaskManager {
+func NewTaskManager(maxUnorganizedBlockCount int) *TaskManager {
 	return &TaskManager{
-		maxTaskCount: maxTaskCount,
-		taskId:       0,
-		pendingFetch: list.New(),
-		inFetch:      make(map[uint64]struct{}),
-		inStage:      make(map[uint64]struct{}),
+		maxUnorganizedBlockCount: maxUnorganizedBlockCount,
+		taskId:                   0,
+		pendingFetch:             orderedmap.NewOrderedMap[uint64, struct{}](),
+		inFetch:                  make(map[uint64]struct{}),
+		inStage:                  make(map[uint64]struct{}),
 	}
 }
 
 func (tm *TaskManager) getTask() (uint64, error) {
+
 	if tm.pendingFetch.Len() != 0 {
 		e := tm.pendingFetch.Front()
-		height := e.Value.(uint64)
+		height := e.Key
 		return height, nil
 	}
 
@@ -39,8 +40,8 @@ func (tm *TaskManager) getTask() (uint64, error) {
 func (tm *TaskManager) popTask() (int, uint64, error) {
 	if tm.pendingFetch.Len() != 0 {
 		e := tm.pendingFetch.Front()
-		height := e.Value.(uint64)
-		tm.pendingFetch.Remove(e)
+		height := e.Key
+		tm.pendingFetch.Delete(height)
 		tm.inFetch[height] = struct{}{}
 		tm.taskId++
 		return tm.taskId, height, nil
@@ -49,24 +50,13 @@ func (tm *TaskManager) popTask() (int, uint64, error) {
 	return 0, 0, xerrors.New("no valid task")
 }
 
-func (tm *TaskManager) clear() {
-	for e := tm.pendingFetch.Front(); e != nil; e = e.Next() {
-		tm.pendingFetch.Remove(e)
-	}
-
-	tm.inFetch = nil
-	tm.inFetch = make(map[uint64]struct{})
-	tm.inStage = nil
-	tm.inStage = make(map[uint64]struct{})
-}
-
 func (tm *TaskManager) extendTask(startHeight uint64, endHeight uint64) error {
 	currentTaskCount := tm.pendingFetch.Len() + len(tm.inFetch) + len(tm.inStage)
-	if currentTaskCount >= tm.maxTaskCount {
+	if currentTaskCount >= tm.maxUnorganizedBlockCount {
 		return xerrors.New("out of maxTaskCount")
 	}
 
-	leftTaskCapacity := uint64(tm.maxTaskCount) - uint64(currentTaskCount)
+	leftTaskCapacity := uint64(tm.maxUnorganizedBlockCount) - uint64(currentTaskCount)
 	gap := endHeight - startHeight
 
 	count := gap
@@ -79,7 +69,7 @@ func (tm *TaskManager) extendTask(startHeight uint64, endHeight uint64) error {
 	for i := uint64(1); i <= count; i++ {
 		h := startHeight + i
 		if tm.pendingFetch.Len() > 0 {
-			existMaxHeight := tm.pendingFetch.Back().Value.(uint64)
+			existMaxHeight := tm.pendingFetch.Back().Key
 			if h <= existMaxHeight {
 				continue
 			}
@@ -93,7 +83,7 @@ func (tm *TaskManager) extendTask(startHeight uint64, endHeight uint64) error {
 			continue
 		}
 
-		tm.pendingFetch.PushBack(h)
+		tm.pendingFetch.Set(h, struct{}{})
 	}
 
 	return nil
@@ -106,8 +96,21 @@ func (tm *TaskManager) fetchSuccess(height uint64) {
 		os.Exit(0)
 	}
 
+	// move to inStage
 	delete(tm.inFetch, height)
 	tm.inStage[height] = struct{}{}
+}
+
+func (tm *TaskManager) fetchFailed(height uint64) {
+	_, exist := tm.inFetch[height]
+	if !exist {
+		fmt.Println("fetch success height not found", height)
+		os.Exit(0)
+	}
+
+	// move to pendingFetch
+	delete(tm.inFetch, height)
+	tm.pendingFetch.Set(height, struct{}{})
 }
 
 func (tm *TaskManager) processSuccess(height uint64) {
@@ -118,4 +121,15 @@ func (tm *TaskManager) processSuccess(height uint64) {
 	}
 
 	delete(tm.inStage, height)
+}
+
+func (tm *TaskManager) clear() {
+	for e := tm.pendingFetch.Front(); e != nil; e = e.Next() {
+		tm.pendingFetch.Delete(e.Key)
+	}
+
+	tm.inFetch = nil
+	tm.inFetch = make(map[uint64]struct{})
+	tm.inStage = nil
+	tm.inStage = make(map[uint64]struct{})
 }
