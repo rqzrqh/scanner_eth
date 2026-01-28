ef6bafb9df87e571213ec4c6afa9f25b8a221668
george <rqzrqh@163.com>
2026-01-28 16:40:00 +0800

    fix balance_erc20

--- model/balance_erc20.go
+++ model/balance_erc20.go
@@ -4,8 +4,8 @@ import "github.com/shopspring/decimal"
 
 type BalanceErc20 struct {
 	Id           uint64          `gorm:"primaryKey:autoIncrement;comment:auto_inc_id"`
-	Addr         string          `gorm:"index;type:varchar(255);comment:addr"`
-	ContractAddr string          `gorm:"index;type:varchar(255);comment:contract_addr"`
+	Addr         string          `gorm:"index;uniqueIndex:addr_contract;type:varchar(255);comment:addr"`
+	ContractAddr string          `gorm:"index;uniqueIndex:addr_contract;type:varchar(255);comment:contract_addr"`
 	Balance      decimal.Decimal `gorm:"type:DECIMAL(65,0);comment:balance"`
 	UpdateHeight uint64          `gorm:"index;type:bigint unsigned;comment:update_height"`
 }
